d2a96c3f917d194f4c4603d6e66f9152c00404fc
martinboca <64015949+martinboca@users.noreply.github.com>
2021-07-18 22:16:56 -0300

    Agregado cargarPaises desde Json:  + CargarJuego class  + Resources folder
    Mejorado "repartirPaises"

--- src/main/java/edu/fiuba/algo3/modelo/Juego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Juego.java
@@ -15,14 +15,6 @@ public class Juego {
     public Juego() {
         jugadores = new ArrayList<>();
         tablero = new Tablero();
-        try {
-            CargarJuego.cargarPaisesAlTablero(tablero, ARCHIVO_PAISES);
-            //CargarJuego.cargarTarjetas(tarjetas, ARCHIVO_TARJETAS);
-        }
-        //Error al cargar los archivos
-        catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void agregarJugador(Jugador unJugador) {
@@ -30,13 +22,16 @@ public class Juego {
     }
 
     public void comenzar() throws CantidadDeJugadoresInsuficienteException, PaisOcupadoPorOtroJugadorException {
+        try {
+            CargarJuego.cargarPaisesAlTablero(tablero, ARCHIVO_PAISES);
+            //CargarJuego.cargarTarjetas(tarjetas, ARCHIVO_TARJETAS);
+        }
+
+        //Error al cargar los archivos
+        catch (Exception e) {e.printStackTrace();}
+
         if (jugadores.size() < 2) throw new CantidadDeJugadoresInsuficienteException();
         else tablero.repartirPaises(jugadores);
-
-    }
-
-    public void repartirPaises() throws PaisOcupadoPorOtroJugadorException {
-        tablero.repartirPaises(jugadores);
 
     }
 
