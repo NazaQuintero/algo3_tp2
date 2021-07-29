f52d6bebd40f1cf8da590e18e65bc205f694db3d
NazaQuintero <45689901+NazaQuintero@users.noreply.github.com>
2021-07-28 21:43:17 -0300

    refactor nesimoCanje

--- src/main/java/edu/fiuba/algo3/modelo/canjes/NesimoCanje.java
+++ src/main/java/edu/fiuba/algo3/modelo/canjes/NesimoCanje.java
@@ -2,13 +2,17 @@ package edu.fiuba.algo3.modelo.canjes;
 
 public class NesimoCanje extends Canje {
 
-    private int cantidadEjercitos;
+    private static int cantidadEjercitos = 10;
 
-    public NesimoCanje(int cantidadEjercitos) { this.cantidadEjercitos = cantidadEjercitos + 5; }
+    public NesimoCanje() { cantidadEjercitos +=  5; }
 
     @Override
     public Canje siguienteCanje() {
-        return new NesimoCanje(this.cantidadEjercitos());
+        return new NesimoCanje();
+    }
+
+    public static void reiniciar() {
+        cantidadEjercitos = 10;
     }
 
     @Override
