9643db1152588da9e41da349e9ac80080f8d9e9f
juanimedone <jmedone@fi.uba.ar>
2021-07-29 18:39:47 -0300

    se ve el campo de juego

--- src/main/java/edu/fiuba/algo3/vista/CampoDeJuego.java
+++ src/main/java/edu/fiuba/algo3/vista/CampoDeJuego.java
@@ -1,11 +1,20 @@
 package edu.fiuba.algo3.vista;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class CampoDeJuego {
+public class CampoDeJuego extends BorderPane {
 
-    public CampoDeJuego() {
+    public CampoDeJuego(Stage stage) {
 
+        ImageView tablero = new ImageView();
         Image image = new Image("tablero.png");
+        tablero.setFitWidth(800);
+        tablero.setFitHeight(600);
+
+        tablero.setImage(image);
+        this.setCenter(tablero);
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/CreacionJugadores.java
+++ src/main/java/edu/fiuba/algo3/vista/CreacionJugadores.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.vista;
 
 import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,20 +14,27 @@ import javafx.stage.Stage;
 public class CreacionJugadores extends BorderPane {
 
     VBox panel = new VBox();
+    VBox cajaDeBotones = new VBox();
+    private static final int ANCHO = 1200;
+    private static final int ALTO = 800;
 
     public CreacionJugadores(Stage stage, int cantidadJugadores) {
 
         this.getStylesheets().add("styles.css");
 
-        for (int i = 0; i < cantidadJugadores; i++) {
+        for (int i = 0; i < 3; i++) {
             Label label = new Label("Ingrese el nombre del jugador " + (i+1) + ": ");
             HBox textFieldHBox = crearTextFieldBox();
-            HBox botonera = crearBotoneraHorizontal();
-            panel.getChildren().addAll(label, textFieldHBox, botonera);
+            Button botonDeCarga = crearBotonDeCarga();
+            panel.getChildren().addAll(label, textFieldHBox, botonDeCarga);
         }
-
+        HBox botonera = crearBotoneraHorizontal(stage);
+        botonera.setSpacing(20);
+        cajaDeBotones.getChildren().addAll(botonera);
+        cajaDeBotones.setAlignment(Pos.BOTTOM_RIGHT);
         panel.setAlignment(Pos.CENTER);
         this.setCenter(panel);
+        this.setRight(cajaDeBotones);
     }
 
     private HBox crearTextFieldBox() {
@@ -38,9 +46,13 @@ public class CreacionJugadores extends BorderPane {
         return textFieldHBox;
     }
 
-    private HBox crearBotoneraHorizontal() {
-        Button buttonSubmit = new Button("Cargar");
+    private HBox crearBotoneraHorizontal(Stage stage) {
+        Button buttonSubmit = new Button("Jugar");
         buttonSubmit.getStyleClass().add("startButton");
+        buttonSubmit.setOnAction(e -> {
+            Scene nuevaEscena = new Scene(new CampoDeJuego(stage), ANCHO, ALTO);
+            stage.setScene(nuevaEscena);
+        });
 
         Button exitButton = new Button("Salir");
         exitButton.getStyleClass().add("exitButton");
