6dbf34fe5fa576e22737e577dd1596561e388ce3
juanimedone <jmedone@fi.uba.ar>
2021-08-09 01:49:35 -0300

    se limpian los dados despues de la ronda de ataque

--- src/main/java/edu/fiuba/algo3/controladores/ReagrupeEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controladores/ReagrupeEventHandler.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.controladores;
 import edu.fiuba.algo3.modelo.excepciones.ElPaisNoEsLimitrofeException;
 import edu.fiuba.algo3.modelo.paises.Pais;
 import edu.fiuba.algo3.vista.CampoDeJuego;
+import edu.fiuba.algo3.vista.MenuLateralDerecho;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
