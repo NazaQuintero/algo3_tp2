568b07da359df5f4ce15683bf961657a2fb9da02
martinboca <64015949+martinboca@users.noreply.github.com>
2021-08-11 15:28:01 -0300

    added button_sound.mp3

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,13 +1,10 @@
 package edu.fiuba.algo3;
 
+import edu.fiuba.algo3.controladores.ReproductorDeSonido;
 import edu.fiuba.algo3.vista.MenuInicial;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
-import java.io.File;
 
 /**
  * JavaFX App
@@ -16,20 +13,15 @@ public class App extends Application {
 
     private static final int ANCHO = 800;
     private static final int ALTO = 550;
-    private static MediaPlayer mediaPlayer;
 
     @Override
     public void start(Stage stage) {
 
-        String musicFile = "src/main/resources/musiquita.mp3";
-        Media musica = new Media(new File(musicFile).toURI().toString());
-        mediaPlayer = new MediaPlayer(musica);
-        mediaPlayer.setVolume(0.1);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        //mediaPlayer.play();
+        ReproductorDeSonido.playPrincipal();
 
         MenuInicial menuInicial = new MenuInicial(stage);
         Scene presentacion = new Scene(menuInicial, ANCHO, ALTO);
+
         stage.setScene(presentacion);
         stage.setTitle("A.L.T.E.G.O.");
         stage.show();
